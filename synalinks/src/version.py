a98c313420b0b62f719f667cc6554a3fec21b676
YoanSallami <sallami.yoan@gmail.com>
2025-08-07 18:37:49 +0200

    Cleanup pyproject and update version

--- synalinks/src/version.py
+++ synalinks/src/version.py
@@ -3,7 +3,7 @@
 from synalinks.src.api_export import synalinks_export
 
 # Unique source of truth for the version number.
-__version__ = "0.3.022"
+__version__ = "0.3.023"
 
 
 @synalinks_export("synalinks.version")
