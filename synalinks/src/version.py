cfe0262b886fecf0dd109b1e12189cb6145213ae
YoanSallami <sallami.yoan@gmail.com>
2025-08-13 12:45:12 +0200

    Update version

--- synalinks/src/version.py
+++ synalinks/src/version.py
@@ -3,7 +3,7 @@
 from synalinks.src.api_export import synalinks_export
 
 # Unique source of truth for the version number.
-__version__ = "0.3.024"
+__version__ = "0.3.025"
 
 
 @synalinks_export("synalinks.version")
